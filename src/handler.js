274b8b7b5d5d706856f593f9556d8a5863f85146
Tatiane A. Nicchetti <tanicchetti@gmail.com>
2022-11-28 13:27:45 -0300

    01) HELLO WORLD: added handler function

---
+++ src/handler.js
@@ -0,0 +1,3 @@
+export const handler = (req, res) => {
+    res.end("hello, Tati")
+}
--- src/server.js
+++ src/server.js
@@ -1,12 +1,9 @@
 import http from "node:http";
+import { handler } from "./handler.js";
 
 const PORT = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-    res.end("hello world")
-})
-
-server.listen(PORT, () => {
+const server = http.createServer(handler).listen(PORT, () => {
     console.log(`connect to ${PORT}...`)
 })
 
